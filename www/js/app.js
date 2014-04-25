e1edd344018713fd1654157c2a92a92a115209f6
beshan <beshan@bigmir.net>
2014-04-25 20:55:19 +0200

    [upd] zoom

--- www/js/app.js
+++ www/js/app.js
@@ -92,7 +92,7 @@ var appInit = function() {
                 id:'panelHolder',
                 fullscreen: true,
                 layout: 'card',
-                activeItem: 2,
+                activeItem: 1,
                 cls:'scaleApp',
                 items: [ {xtype:'startPage'}, {xtype:'mainPage'}, {xtype:'planTripPage'},
                     {xtype:'aboutPage'}, {xtype:'settingsPage'} ]
