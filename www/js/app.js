6928fc1b18e704d9643089e1d329ac499918a780
beshan <beshan@bigmir.net>
2014-04-25 20:34:59 +0200

    [upd] zoom

--- www/js/app.js
+++ www/js/app.js
@@ -28,7 +28,7 @@ Ext.define('App.view.MainPage' ,{
                     }
                 }]
             },{
-                html: '<div class="page"><br><br>Page content 222</div>'
+                html: '<div class="page"><br><br>Page content 333</div>'
             }
         ]
     }
