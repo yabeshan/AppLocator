f9ff5749f8cb3f042cb5be3ea028c03ddf84883a
beshan <beshan@bigmir.net>
2014-04-30 01:23:22 +0200

    a

--- www/js/view/panel/ChangeInfoPanel.js
+++ www/js/view/panel/ChangeInfoPanel.js
@@ -21,7 +21,7 @@ Ext.define('App.view.TestPanel' ,{
                 }
             },{
                 id:'menuList2',
-                cls:'menu-zoomer',
+                cls:'info-zoom',
                 html: '<div class="main-page-menu-panel">'
                     + '<div id="homeBtn" class="button main-page-menupanel-button main-page-menupanel-logo"></div>'
 
