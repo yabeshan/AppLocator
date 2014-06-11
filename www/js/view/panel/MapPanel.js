6a1eb04176622393a1dfc89ea830e32e368464e5
7idbiz <beshan@bigmir.net>
2014-06-11 09:28:18 +0200

    retest mousedown

--- www/js/view/panel/MapPanel.js
+++ www/js/view/panel/MapPanel.js
@@ -51,6 +51,19 @@ Ext.define('App.view.MapPanel', {
         } );
 
         this.gMap = map.getMap();
+
+        setInterval(function(){
+            var obj1 = document.getElementsByClassName("pac-container");
+            var obj2 = document.getElementsByClassName("pac-item");
+            if (obj1[0]) {
+                obj1[0].addEventListener('mousedown', function(ev) {
+//                    console.log( ev.currentTarget.innerHTML );
+//                    ev.currentTarget.innerHTML
+                    alert( 111 );
+                }, false);
+            }
+        },5000);
+        
     },
 
     completeMap: function(extMapComponent, googleMapComp) {
