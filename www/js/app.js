8ce29fcc334a85eb17f4f48b0635da0c23731a06
beshan <beshan@bigmir.net>
2014-09-16 18:05:12 +0300

    ajax

--- www/js/app.js
+++ www/js/app.js
@@ -82,7 +82,7 @@ var onDeviceReady = function() {
 Ext.Ajax.request({
     url: urlConfig,
     callback: function(response, qwe, asd) {
-        alert(response +"   "+ qwe +"    "+ asd);
+        alert(response.UtcDateTime +"   "+ qwe +"    "+ asd.UtcDateTime );
 //        console.log ("***************", response);
     },
     failure: function(response) {
