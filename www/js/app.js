c3232b6b5938bbe860c8af8cd3a0ca8cc219596b
beshan <beshan@bigmir.net>
2014-09-18 14:21:40 +0300

    upd

--- www/js/app.js
+++ www/js/app.js
@@ -77,7 +77,25 @@ var onDeviceReady = function() {
 
     document.addEventListener("offline", onOffline, false);
     document.addEventListener("online", onLine, false);
+
+    checkConnection();
 };
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
 
 function onLine() {
     alert("online");
